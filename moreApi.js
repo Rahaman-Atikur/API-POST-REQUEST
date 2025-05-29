849f2e27c2a4fcd295b3fe77ea927f2382cc3b0e
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-30 00:33:13 +0100

    dynamic data showing on Websites

--- moreApi.js
+++ moreApi.js
@@ -3,5 +3,10 @@ let handleLoadUser =()=>{
     .then(response=>response.json())
     .then(data=> {
         console.log(data);
+        displayUser(data);
     })
+}
+
+let displayUser = (users) =>{
+    console.log(users[0]);
 }
