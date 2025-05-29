5f33017a5c855961250760c8f4dad61823816671
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-29 22:13:51 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let shops={
+    productName:"Rice",
+    productPrice:14,
+    productAvailability:true,
+    productLocation:"atShop"
+}
+let stringShops=JSON.stringify(shops);
+console.log( typeof stringShops);
