6126da03a11e4ff42e7101f76142d97f2335ab48
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-30 22:28:30 +0100

    Error Handleing

---
+++ async.js
@@ -0,0 +1,8 @@
+let handleUser = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response=>response.json())
+    .then(data =>console.log(data))
+    .catch(error=>console.log("Error Found"))
+};
+
+handleUser();
